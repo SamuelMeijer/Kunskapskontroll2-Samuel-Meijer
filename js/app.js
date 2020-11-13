ebd0ff64b23646f3e0233e9cbda7b1d65db85685
SamuelMeijer <samuel.j.meijer@gmail.com>
2020-11-13 11:32:13 +0100

    Adding instructions for the assignment

---
+++ js/app.js
@@ -0,0 +1,14 @@
+/*
+**** INSTRUCTIONS ****
+1. All info ska presenteras i metric units
+2. Ett formulär med en text-input och en submit-knapp där användaren anger en stad.
+3. Det nuvarande vädret för den inmatade staden hämtas och  presenteras. Du behöver ha med följande: 
+    - Description
+    - Väderikon, se https://openweathermap.org/weather-conditions 
+    - Temperatur
+    - Vindhastighet
+    - Luftfuktighet
+4. Temperaturdatan ska användas för att ändra färg på något i appen. Kan vara hela bakgrunden eller en liten ikon, du väljer själv.
+5. Utifall den inmatade staden inte kan hittas ska det visas ett tydligt och informerande meddelande.
+6. Kommentera all kod med beskrivning av vad den gör
+*/
