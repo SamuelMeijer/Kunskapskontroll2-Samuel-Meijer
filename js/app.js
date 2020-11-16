0fd89b6099e8c8a49ef16e14eb2e58a0e69d58f7
SamuelMeijer <samuel.j.meijer@gmail.com>
2020-11-16 15:33:54 +0100

    Targeting elements and adding API-request with the event 'submit'

--- js/app.js
+++ js/app.js
@@ -12,3 +12,38 @@
 5. Utifall den inmatade staden inte kan hittas ska det visas ett tydligt och informerande meddelande.
 6. Kommentera all kod med beskrivning av vad den gör
 */
+
+// ***** ELEMENTS *****
+const form = document.getElementById('header-form');
+const formInput = document.getElementById('header-form-input');
+const formBtn = document.getElementById('header-form-btn');
+
+const mainContentContainer = document.querySelector('.main-content-container');
+const mainTitle = document.querySelector('.main-title');
+const mainDescription = document.querySelector('.main-description');
+
+// ***** API-URL *****
+// TODO: Uncomment, dont want to waste API-calls during development of other parts since there is a limit of 1000/day.
+const baseUrl = 'http://api.openweathermap.org/data/2.5/weather';
+const apiKey = 'dfd0919a1a33da253891307cacb8266c';
+
+// ***** EVENT LISTENERS *****
+form.addEventListener('submit', function (event) {
+    let userInput = formInput.value;
+
+    // TODO: Add alternatives for units and language? Currently units will be displayed as metric and language as swedish.
+    fetch(`${baseUrl}?q=${userInput}&units=metric&lang=sv&appid=${apiKey}`).then(handleResponse).catch(function (error) {
+        console.error('FEEEEL', error.message);
+    });
+
+    // Preventing normal submit-behaviour.
+    event.preventDefault();
+});
+
+// ***** FUNCTIONS *****
+async function handleResponse (promise) {
+    console.log(promise);
+    // TODO: Add try...catch
+    const promiseJs = await promise.json();
+    console.log(promiseJs);
+};
