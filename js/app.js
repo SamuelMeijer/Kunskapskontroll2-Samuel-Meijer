9cd4153be2e023a50d4a5b94126cff4c6edbf9e6
SamuelMeijer <samuel.j.meijer@gmail.com>
2020-11-23 15:34:39 +0100

    Adding errorhandling

--- js/app.js
+++ js/app.js
@@ -43,22 +43,75 @@ form.addEventListener('submit', function (event) {
 
 // ***** FUNCTIONS *****
 async function handleResponse (promise) {
-    console.log(promise);
-    // TODO: Add try...catch
-    const promiseJs = await promise.json();
-    console.log(promiseJs);
-
-    /* *** Updating textcontent. *** */
+    // Storing the users input in a variable and reseting the <input>-element to allow new searches.
     let userInput = formInput.value;
-    // Calling function 'formatString' to make sure the data will be presented in a proper format.
+    // Calling function 'formatString' to make sure the data from userInput will be presented in a proper format.
     userInput = formatString(userInput);
-    
-    // Reseting the input-element (searchfield).
+    // Reseting the <input>-element (searchfield).
     formInput.value = '';
-    // TODO: REMOVE? Using .blur() to remove the focus from the input-element (searchfield) in order to remove search suggestions for old user-input from showing, .focus() is used to once again focus on the input-element (searchfield) after the old user-input has been removed.
+    // Using .blur() to remove the focus from the input-element (searchfield) in order to remove search suggestions for old user-input from showing
     formInput.blur();
+    // Using .focus() to once again focus on the input-element (searchfield) after the old user-input has been removed.
     formInput.focus();
 
+    // TODO: REMOVE!
+    console.log('PROMISE:', promise);
+
+    // Evaluating if the response from the API was ok or if an error occurred.
+    if (!promise.ok) {
+        // Hiding the <table>-element that displays weather information if one exists.
+        let table = document.querySelector('.main-table');
+        if (table) {
+            table.style.display = 'none';
+        };
+
+        // Updating textcontent to let the user know an error has occurred. 
+        mainTitle.textContent = 'Fel :(';
+        // Evaluates if the statuscode is 404, which means a city with the searched name does not exist in the API.
+        if (promise.status === 404) {
+            // Updating the textcontent to let the user know no such city was found.
+            mainDescription.textContent = `Vi lyckas tyvärr inte hitta en stad med namnet: ${userInput}`;
+            // Throwing an error.
+            throw new Error(`An error occurred while searching for a city with the name ${userInput}: ` + promise.status + ' ' + promise.statusText);
+        } else {
+            // Updating the textcontent to let the user know something went wrong.
+            mainDescription.textContent = 'Något gick tyvärr fel med sökningen';
+            // Throwing an error.
+            throw new Error('An error occurred while communicating with the server: ' + promise.status + ' ' + promise.statusText);
+        }
+    };
+    
+    // Defining a variable that will hold the data in the response.
+    let promiseJs = null;
+    // Checking for errors while converting the data in the response from JSON to JavaScript.
+    try {
+        // Converting the data in the response from JSON to JavaScript and storing it in 'promiseJs'.
+        promiseJs = await promise.json();
+
+        // Evaluating if the data is complete or if something is missing.
+        if (!promiseJs.weather[0].description) {
+            throw new SyntaxError(`Incomplete data: No weather description`);
+        } else if (!promiseJs.weather[0].icon) {
+            throw new SyntaxError(`Incomplete data: No weather icon`);
+        } else if (!promiseJs.main.temp) {
+            throw new SyntaxError(`Incomplete data: No temperature`);
+        } else if (!promiseJs.main.feels_like) {
+            throw new SyntaxError(`Incomplete data: No feels like temperature`);
+        } else if (!promiseJs.wind.speed) {
+            throw new SyntaxError(`Incomplete data: No wind speed`);
+        } else if (!promiseJs.wind.deg) {
+            throw new SyntaxError(`Incomplete data: No wind direction`);
+        } else if (!promiseJs.main.humidity) {
+            throw new SyntaxError(`Incomplete data: No humidity`);
+        }
+    } catch (error) {
+        console.error('Json Error: ' + error);
+    }
+
+    //TODO: REMOVE!
+    console.log(promiseJs)
+    
+    /* *** Updating textcontent. *** */
     // Updating textcontent with userInput instead of directly getting the name from API-response because the API-response is sometimes in english and sometimes in swedish.
     mainTitle.textContent = userInput;
     mainDescription.textContent = `I ${userInput} är det just nu ${promiseJs.weather[0].description}`;
@@ -69,6 +122,10 @@ async function handleResponse (promise) {
    }
 
    /* *** Updating <table>-element to display weather information *** */
+   let table = document.querySelector('.main-table');
+   // Setting display to block to make sure it is visible, if the user previously has done a search on a city that cannot be found display will be set to 'none'.
+   table.style.display = 'block';
+   
    // Updating the <img>-element that is a child of <thead> to display a weather icon.
    let weatherImg = document.querySelector('table thead th img');
    weatherImg.setAttribute('src', `http://openweathermap.org/img/wn/${promiseJs.weather[0].icon}@2x.png`);
@@ -102,6 +159,11 @@ async function handleResponse (promise) {
    temperatureColor(promiseJs.main.temp, trNodeList[0].children[1]);
    temperatureColor(promiseJs.main.feels_like, trNodeList[1].children[1]);
 };
+
+// Handling errors related to the response from the API or the content of the response.
+function errorHandler (error) {
+    console.error(error);
+}
 
 // addTable creates a <table>-element with children and appends it as a child to mainContentContainer.
 function addTable () {
@@ -142,12 +204,6 @@ function temperatureColor (temperature, element) {
         element.style.color = 'green';
     };
 };
-
-// Handling errors related to the response from the API or the content of the response.
-function errorHandler (error) {
-    // TODO: ADD FUNCTIONALITY!
-    console.error('FEEEEL', error.message);
-}
 
 // Converts the first character of every new word in a string to uppercase, every other character in the string will be lowercase. 
 // ex) 'riO de jAnEiro' -> 'Rio De Janeiro'.
